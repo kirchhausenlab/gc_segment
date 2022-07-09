af44f84c820742b1a60d318ad9b71267b513b31e
Benjamin Gallusser <bgallusser@googlemail.com>
2022-07-09 19:32:39 +0200

    Clean up repo

--- code/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python segment.py --cfg configs/20190613_ER_0001_tom.yaml
-python segment.py --cfg configs/20190613_ER_0003_tom.yaml
-python segment.py --cfg configs/20190613_mitochondrion_0010_tjv.yaml
-python segment.py --cfg configs/20190613_mitochondrion_0020_tjv.yaml
-python segment.py --cfg configs/20190613_mitochondrion_0030_tjv.yaml
-python segment.py --cfg configs/20190613_mitochondrion_0040_tjv.yaml
